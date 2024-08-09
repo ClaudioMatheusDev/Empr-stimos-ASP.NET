e5d66759bcf1f394dd9c8b2d23651e41ddb42241
Claudio Matheus <claudiomatheus055@gmail.com>
2024-08-08 23:40:46 -0300

    iniciando o projeto

---
+++ EmprestimoLivros/Models/EmprestimosModel.cs
@@ -0,0 +1,12 @@
+﻿namespace EmprestimoLivros.Models
+{
+    public class EmprestimosModel
+    {
+        public int Id { get; set; }
+        public string Recebedor { get; set; }
+        public string Fornecedor { get; set; }
+        public string LivroEmprestado { get; set; }
+        public DateTime DataEmprestimo { get; set; } = DateTime.Now;
+
+    }
+}
