7d28fe4d4d325edd580108331956807bf6df8a96
Claudio Matheus <claudiomatheus055@gmail.com>
2024-08-22 09:06:46 -0300

    ADD: CONTRROLE = EmprestimoController and view Emprestimos - INDEX.CSHTML

---
+++ EmprestimoLivros/Controllers/EmprestimoController.cs
@@ -0,0 +1,22 @@
+﻿using EmprestimoLivros.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmprestimoLivros.Controllers
+{
+    public class EmprestimoController : Controller
+    {
+
+        readonly private ApplicationDbContext _db; //CRIANDO UMA PROPIEDADE PRIVADA 
+
+        public EmprestimoController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
